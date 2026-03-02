a0f24afa66d6800f981b992091c38d699fa43a72
Danail Krzhalovski <dkrzhalovski.apps@gmail.com>
2026-03-02 10:13:16 +0100

    update open obsidian script

--- Scripts/open-obsidian.sh
+++ Scripts/open-obsidian.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Switch to workspace 3 (index 2), then open Obsidian
 xdotool set_desktop 3
-flatpak run md.obsidian.Obsidian &
+flatpak run md.obsidian.Obsidian >>/dev/null 2>&1 &
