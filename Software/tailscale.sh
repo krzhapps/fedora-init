f82641a445a1a90179b95d7aee3380ff0fa0334c
Danail Krzhalovski <dkrzhalovski.apps@gmail.com>
2026-03-02 12:13:30 +0100

    enable ssh

--- Software/tailscale.sh
+++ Software/tailscale.sh
@@ -5,12 +5,15 @@ echo "==> Installing Tailscale..."
 
 # Add the official Tailscale repository and install
 sudo dnf config-manager addrepo \
-    --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo \
-    --overwrite
+  --from-repofile=https://pkgs.tailscale.com/stable/fedora/tailscale.repo \
+  --overwrite
 
 sudo dnf install -y tailscale
 
 # Enable and start the daemon
 sudo systemctl enable --now tailscaled
+
+# Enable SSH management by tailscale
+sudo tailscale set --ssh
 
 echo "==> Tailscale installed. Run 'sudo tailscale up' to authenticate."
