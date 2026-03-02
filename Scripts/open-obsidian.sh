5733aad728367cb735a02978016be5b525130e4c
Danail Krzhalovski <dkrzhalovski.apps@gmail.com>
2026-03-02 10:02:46 +0100

    updated obsidian shortcut

--- Scripts/open-obsidian.sh
+++ Scripts/open-obsidian.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 # Switch to workspace 3 (index 2), then open Obsidian
-gdbus call --session \
-  --dest org.gnome.Shell \
-  --object-path /org/gnome/Shell \
-  --method org.gnome.Shell.Eval \
-  "global.workspace_manager.get_workspace_by_index(2).activate(global.get_current_time())"
+xdotool set_desktop 3
 flatpak run md.obsidian.Obsidian &
--- Software/shortcuts.sh
+++ Software/shortcuts.sh
@@ -5,6 +5,14 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 echo "==> Configuring keyboard shortcuts..."
 
+# Deploy helper scripts
+mkdir -p "$HOME/.local/bin"
+install -m 755 "$SCRIPT_DIR/../Scripts/open-obsidian.sh" "$HOME/.local/bin/open-obsidian"
+
+# Clear conflicting system keybindings
+# Super+O defaults to rotate-video-lock-static which takes priority over custom bindings
+gsettings set org.gnome.settings-daemon.plugins.media-keys rotate-video-lock-static "['XF86RotationLockToggle']"
+
 CUSTOM_BASE="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
 
 # Register all custom keybinding slots
@@ -62,6 +70,11 @@ gsettings set $WM move-to-workspace-left "['<Super><Shift>Left']"
 gsettings set $WM move-to-workspace-right "['<Super><Shift>Right']"
 
 # SUPER+O -> Obsidian on workspace 3
-xdotool set_desktop 3 && flatpak run md.obsidian.Obsidian
+gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${CUSTOM_BASE}/custom2/" \
+  name "Open Obsidian"
+gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${CUSTOM_BASE}/custom2/" \
+  command "$HOME/.local/bin/open-obsidian"
+gsettings set "org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:${CUSTOM_BASE}/custom2/" \
+  binding "<Super>o"
 
 echo "==> Keyboard shortcuts configured."
