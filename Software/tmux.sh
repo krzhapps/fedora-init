02d87888632e24dba5c37eff81546b5a4796d31c
Danail Krzhalovski <dkrzhalovski.apps@gmail.com>
2026-03-23 19:08:38 +0100

    use correct directory and name

--- Software/tmux.sh
+++ Software/tmux.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 CONFIG_DIR="$HOME/.config/alacritty"
 
-cp "$SCRIPT_DIR/../Configs/.tmux.conf" "$CONFIG_DIR/.tmux.toml"
+cp "$SCRIPT_DIR/../Configs/.tmux.conf" "$CONFIG_DIR/.tmux.conf"
