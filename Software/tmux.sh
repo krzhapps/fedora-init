d34666f56e64b7e13e95c410b5fb1e154514aa62
Danail Krzhalovski <dkrzhalovski.apps@gmail.com>
2026-03-23 19:09:08 +0100

    simplify script

--- Software/tmux.sh
+++ Software/tmux.sh
@@ -2,6 +2,5 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-CONFIG_DIR="$HOME/.config/alacritty"
 
-cp "$SCRIPT_DIR/../Configs/.tmux.conf" "$CONFIG_DIR/.tmux.conf"
+cp "$SCRIPT_DIR/../Configs/.tmux.conf" "$HOME/.tmux.conf"
