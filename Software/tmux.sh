3b5755148f5743213e3caa0dd887847f02064dea
Danail Krzhalovski <dkrzhalovski.apps@gmail.com>
2026-04-24 15:04:18 +0200

    updated tmux to have a persistent session manager

--- Software/tmux.sh
+++ Software/tmux.sh
@@ -2,5 +2,6 @@
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp "$SCRIPT_DIR/../Configs/.tmux.conf" "$HOME/.tmux.conf"
