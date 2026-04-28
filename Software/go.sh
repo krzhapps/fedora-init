bbb47b24bf14717325442a67fd2064086b2203ac
Danail Krzhalovski <dkrzhalovski.apps@gmail.com>
2026-04-28 18:45:21 +0200

    install golangci v2

--- Software/go.sh
+++ Software/go.sh
@@ -15,7 +15,7 @@ GOPATH_LINE='export PATH="$PATH:$(go env GOPATH)/bin"'
 GUARD_COMMENT='# go tools path'
 
 if ! grep -qF "$GUARD_COMMENT" "$HOME/.bashrc"; then
-  printf '\n%s\n%s\n' "$GUARD_COMMENT" "$GOPATH_LINE" >> "$HOME/.bashrc"
+  printf '\n%s\n%s\n' "$GUARD_COMMENT" "$GOPATH_LINE" >>"$HOME/.bashrc"
   echo "==> Added GOPATH/bin to ~/.bashrc"
 else
   echo "==> GOPATH/bin already in ~/.bashrc, skipping."
@@ -23,7 +23,7 @@ fi
 
 if [ -f "$HOME/.zshrc" ]; then
   if ! grep -qF "$GUARD_COMMENT" "$HOME/.zshrc"; then
-    printf '\n%s\n%s\n' "$GUARD_COMMENT" "$GOPATH_LINE" >> "$HOME/.zshrc"
+    printf '\n%s\n%s\n' "$GUARD_COMMENT" "$GOPATH_LINE" >>"$HOME/.zshrc"
     echo "==> Added GOPATH/bin to ~/.zshrc"
   else
     echo "==> GOPATH/bin already in ~/.zshrc, skipping."
@@ -34,7 +34,7 @@ echo "==> Installing Go tools..."
 
 export PATH="$PATH:$(go env GOPATH)/bin"
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@latest
 go install github.com/krzhapps/GithubTickets/cmd/tickets@latest
 
 echo "==> Go installed and configured."
